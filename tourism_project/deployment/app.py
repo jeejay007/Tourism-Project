9bff62c57c320242b9bb0dfe5cb89dc4d4377dec
Gautham Jayatheertha <60561140+jeejay007@users.noreply.github.com>
2026-04-05 19:25:22 +0530

    Update model filename in app.py

--- tourism_project/deployment/app.py
+++ tourism_project/deployment/app.py
@@ -11,7 +11,7 @@ from huggingface_hub import hf_hub_download
 
 # --- 1. GLOBAL CONFIGURATION ---
 REPO_ID = "GauthamJ007/VisitWithUs-Wellness-Tourism-Predictor"
-FILENAME = "xgboost_model.joblib"
+FILENAME = "tourism_pipeline.joblib"
 
 # --- 2. BACKEND: FastAPI Setup ---
 app = FastAPI(title="Visit with Us - Backend API")
