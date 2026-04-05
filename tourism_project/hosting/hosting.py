9f0b7710e9f2fb792b5adb5bcb7de4c987f46e2c
Gautham Jayatheertha <60561140+jeejay007@users.noreply.github.com>
2026-04-05 13:50:21 +0530

    Refactor deployment to use upload_folder method
    Updated the deployment method to use upload_folder for easier file uploads.

--- tourism_project/hosting/hosting.py
+++ tourism_project/hosting/hosting.py
@@ -1,26 +1,10 @@
-
 from huggingface_hub import HfApi
 import os
 
 api = HfApi(token=os.getenv("HF_TOKEN"))
-
-# Replace with your Space name and Username
-SPACE_ID = "GauthamJ007/VisitWithUs-Wellness-Tourism-Project-Space"
-
-# Create the Space if it doesn't exist (SDK/Docker type)
-api.create_repo(repo_id=SPACE_ID, repo_type="space", space_sdk="docker", exist_ok=True)
-
-# Define Google Drive deployment path
-folder_path="/content/drive/MyDrive/Project10/tourism_project/deployment"
-
-files_to_upload = ["app.py", "requirements.txt", "Dockerfile"]
-
-for file in files_to_upload:
-    api.upload_file(
-        path_or_fileobj=os.path.join(folder_path, file),
-        path_in_repo=file,
-        repo_id=SPACE_ID,
-        repo_type="space"
-    )
-
-print(f"Deployment successful! Access your API at: https://huggingface.co/spaces/{SPACE_ID}")
+api.upload_folder(
+    folder_path="tourism-project/deployment", # the local folder containing your files
+    repo_id="GauthamJ007/VisitWithUs-Wellness-Tourism-Project-Space",   # the target repo
+    repo_type="space",                        # dataset, model, or space
+    path_in_repo="",                          # optional: subfolder path inside the repo
+)
